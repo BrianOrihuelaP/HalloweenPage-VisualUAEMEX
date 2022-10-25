7abd1c44d93f1a6d84a0d91d39d986a85ba0c36f
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-10-25 18:31:04 -0500

    Agregando audio

---
+++ codigo/script0.js
@@ -0,0 +1,35 @@
+
+function play() {
+    // en esta funcion creamos una variable la cual sera de tipo audio
+    let playMe
+    playMe = new Audio('../audio/screamW1.mp3'); //cargamos el archivo de audio
+    playMe.preload = "auto";
+    playMe.volume = .7;
+    playMe.play();
+    // Aqui recargamos hacia otra pagina
+    setTimeout(redirigir, 2800);
+}
+
+function play2() {
+    let playMe
+    playMe = new Audio('../audio/KidFX.mp3');
+    playMe.preload = "auto";
+    playMe.volume = .8;
+    playMe.play();
+    console.log("play function");
+    // Aqui recargamos hacia otra pagina
+    setTimeout(redirigir2, 2800);
+}
+
+function redirigir2() {
+    window.location.href = "../moviesA.html";
+}
+
+function redirigir() {
+    window.location.href = "../movies.html";
+}
+
+
+
+
+
